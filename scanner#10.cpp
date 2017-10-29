f4881e3878655de4572507eb29df1bd91c0beb2f
cvtran <cvtran31@gmail.com>
2017-10-29 14:05:56 -0700

    Update scanner#10.cpp

--- scanner#10.cpp
+++ scanner#10.cpp
@@ -3,72 +3,75 @@
 
 //Returns true if one of the reserved words is found and prints out the token type and word.
 bool reservedWord(string input) {
-	if (input == "masu") {
+	if (input == ".") {
+		cout << "Token Type: PERIOD		Word is: " << input << endl;
+		return true;
+	}
+	else if (input == "masu") {
 		cout << "Token Type: VERB		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "masen") {
 		cout << "Token Type: VERBNEG		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "mashita") {
 		cout << "Token Type: VERBPAST		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "masendeshita") {
 		cout << "Token Type: VERBPASTNEG	Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "desu") {
 		cout << "Token Type: IS			Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "deshita") {
 		cout << "Token Type: WAS		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "o") {
 		cout << "Token Type: OBJECT		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "wa") {
 		cout << "Token Type: SUBJECT		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "ni") {
 		cout << "Token Type: DESTINATION	Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "watashi" || input == "anata" || input == "kare" || input == "sore") {
 		cout << "Token Type: PRONOUN		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "mata" || input == "soshite" || input == "shikashi" || input == "dakara") {
 		cout << "Token Type: CONNECTOR		Word is: " << input << endl;
 		return true;
 	}
-
 	else if (input == "eofm") {
 		cout << "Token Type: EOFM		Word is: " << input << endl;
 		return true;
 	}
-
 	else
 		return false;
 }
 
 int main ()
 {
+	string input;
+	
+	if (!reservedWord(input))
+		if (scanner(input)) {
+			char lastChar = input[input.length - 1];
+			if ((lastChar == 'a') || (lastChar == 'e') || (lastChar == 'i') || (lastChar == 'o') || (lastChar == 'u'))
+				cout << "Token Type: WORD1		Word is: " << input << endl;
+			else
+				cout << "Token Type: WORD2		Word is: " << input << endl;
+		}
+		
 
-  return 0;
+	return 0;
 }
