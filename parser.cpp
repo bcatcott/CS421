ba534b451f8e9c9443db4b3f0ab4439ff4de02a7
So Cal <bc@catcott.com>
2017-11-28 14:01:28 -0800

    debug enum

--- parser.cpp
+++ parser.cpp
@@ -12,16 +12,15 @@ using namespace std;
 // ** Be sure to put the name of the programmer above each function
 // i.e. Done by:
 
-enum token_type { ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR };
 
 string tokens[15] = { "ERROR", "WORD1", "WORD2", "PERIOD", "VERB", "VERBNEG", "VERBPAST", "VERBPASTNEG", "IS", "WAS", "OBJECT", "SUBJECT", "DESTINATION", "PRONOUN", "CONNECTOR" };
 
 token_type  saved_token;     // global buffer for the scanner token
 
-string saved_lexeme;		// global buffer for the saved lexeme
+string saved_lexeme;// global buffer for the saved lexeme
 ifstream fin;               //global file
-							// global flag indicating whether
-							// we have saved a token to eat up or not
+// global flag indicating whether
+// we have saved a token to eat up or not
 bool   token_available;
 
 
@@ -30,18 +29,18 @@ bool   token_available;
 // Done by: Brad
 void syntaxerror1(token_type expected, string lexeme)
 {
-	cout << "SYNTAX ERROR: expected " << tokens[expected] << " but found " << lexeme << endl;
+  cout << "SYNTAX ERROR: expected " << tokens[expected] << " but found " << lexeme << endl;
 }
 
 // Done by: Brad
-void syntaxerror2(string lexeme, string parserFunction)
+void syntaxerror2 (string lexeme, string parserFunction)
 {
-	cout << "SYNTAX ERROR: unexpected " << lexeme << " found in " << parserFunction << endl;
+  cout << "SYNTAX ERROR: unexpected " << lexeme << " found in " << parserFunction << endl;
 }
 
 // ** Need the updated match and next_token (with 2 global vars)
 
-//	next_token(void)
+//next_token(void)
 //    Looks ahead to see what token comes next from the scanner.
 //    HOW: checks first to see if the token_available flag is false.
 //    If so, saved_token gets scanner() result.
@@ -52,16 +51,16 @@ void syntaxerror2(string lexeme, string parserFunction)
 // Done by: Cam
 token_type next_token()
 {
-	string lexeme;
-    scanner scan;
-	if (!token_available)   // if there is no saved token from previous lookahead
-	{
-		if (scan.scan(saved_token, lexeme, fin) == 0)	 // call scanner to grab a new token
-			cout << "Lexical error found " << lexeme << endl;
-		token_available = true;                  // mark that fact that you have saved it
-		saved_lexeme = lexeme;
-	}
-	return saved_token;    // return the saved token
+  string lexeme;
+  scanner scan;
+  if (!token_available)   // if there is no saved token from previous lookahead
+    {
+      if (scan.scan(saved_token, lexeme, fin) == 0) // call scanner to grab a new token
+	cout << "Lexical error found " << lexeme << endl;
+      token_available = true;                  // mark that fact that you have saved it
+      saved_lexeme = lexeme;
+    }
+  return saved_token;    // return the saved token
 }
 
 //match(expected)
@@ -72,16 +71,16 @@ token_type next_token()
 // Done by: Cam
 bool match(token_type expected)
 {
-	if (next_token() != expected)  // mismatch has occurred with the next token
-	{ // generate a syntax error message here
-	  // do error handling here if any
-		syntaxerror1(expected, saved_lexeme);
-	}
-	else  // match has occurred
-	{
-		token_available = false;  // eat up the token
-		return true;              // say there was a match
-	}
+  if (next_token() != expected)  // mismatch has occurred with the next token
+    { // generate a syntax error message here
+      // do error handling here if any
+      syntaxerror1(expected, saved_lexeme);
+    }
+  else  // match has occurred
+    {
+      token_available = false;  // eat up the token
+      return true;              // say there was a match
+    }
 }
 
 
@@ -93,25 +92,25 @@ bool match(token_type expected)
 // Done by: Cam
 void story()
 {
-	cout << "Processing <story>" << endl;
-	s();
-	while (true)
+  cout << "Processing <story>" << endl;
+  s();
+  while (true)
+    {
+      switch (next_token()) // look ahead to see if the valid start is there
 	{
-		switch (next_token()) // look ahead to see if the valid start is there
-		{
-			case CONNECTOR: s();		// found another beginning 
-										// so do it
-							break;
-			case WORD1:		s();		// found another beginning 
-										// so do it
-							break;
-			case PRONOUN:	s();		// found another beginning 
-										// so do it
-							break;
-			default: 
-				return;  // loop stops
-		}//end switch
-	}//end loop
+	case CONNECTOR: s();// found another beginning 
+	  // so do it
+	  break;
+	case WORD1:s();// found another beginning 
+	  // so do it
+	  break;
+	case PRONOUN:s();// found another beginning 
+	  // so do it
+	  break;
+	default: 
+	  return;  // loop stops
+	}//end switch
+    }//end loop
 }
 
 
@@ -119,37 +118,37 @@ void story()
 // Done by: Cam
 void s()
 {
-	cout << "Processing <s>" << endl;
-	if (next_token() == CONNECTOR)    // start of what is in [ ] is found
-		match(saved_token);			// do it
+  cout << "Processing <s>" << endl;
+  if (next_token() == CONNECTOR)    // start of what is in [ ] is found
+    match(saved_token);// do it
 
-	noun();
-	match(token_type::SUBJECT);
-	s1();
+  noun();
+  match(token_type::SUBJECT);
+  s1();
 }
 
 // 3 <s1> :: = <verb> <tense> PERIOD | <noun> <s2>
 // Done by: Cam
 void s1()
 {
-	cout << "Processing <s1>" << endl;
-	switch (next_token())
-	{
-	case WORD2:
-		verb();
-		tense();
-		match(token_type::PERIOD);
-		break;
+  cout << "Processing <s1>" << endl;
+  switch (next_token())
+    {
+    case WORD2:
+      verb();
+      tense();
+      match(token_type::PERIOD);
+      break;
 
-	case WORD1:
-	case PRONOUN:
-		noun();
-		s2();
-		break;
+    case WORD1:
+    case PRONOUN:
+      noun();
+      s2();
+      break;
 
-	default:
-		syntaxerror2(saved_lexeme, "s1");
-	}
+    default:
+      syntaxerror2(saved_lexeme, "s1");
+    }
 
 }
 
@@ -157,133 +156,133 @@ void s1()
 // Done by: Brad
 void s2()
 {
-	cout << "Processing <s2>" << endl;
-	switch (next_token())
-	{
-	case IS:
-	case WAS:
-		be();
-		match(token_type::PERIOD);
-		break;
+  cout << "Processing <s2>" << endl;
+  switch (next_token())
+    {
+    case IS:
+    case WAS:
+      be();
+      match(token_type::PERIOD);
+      break;
 
-	case DESTINATION:
-		match(token_type::DESTINATION);
-		verb();
-		tense();
-		match(token_type::PERIOD);
-		break;
+    case DESTINATION:
+      match(token_type::DESTINATION);
+      verb();
+      tense();
+      match(token_type::PERIOD);
+      break;
 
-	case OBJECT:
-		match(token_type::OBJECT);
-		s3();
-		break;
+    case OBJECT:
+      match(token_type::OBJECT);
+      s3();
+      break;
 
-	default:
-		syntaxerror2(saved_lexeme, "s2");
-	}
+    default:
+      syntaxerror2(saved_lexeme, "s2");
+    }
 }
 
 // 5 <s3> :: = <verb> <tense> PERIOD | <noun> DESTINATION <verb> <tense> PERIOD
 // Done by: Brad
 void s3()
 {
-	cout << "Processing <s3>" << endl;
-	switch (next_token())
-	{
-	case WORD2:
-		verb();
-		tense();
-		match(token_type::PERIOD);
-		break;
+  cout << "Processing <s3>" << endl;
+  switch (next_token())
+    {
+    case WORD2:
+      verb();
+      tense();
+      match(token_type::PERIOD);
+      break;
 
-	case WORD1:
-	case PRONOUN:
-		noun();
-		match(token_type::DESTINATION);
-		verb();
-		tense();
-		match(token_type::PERIOD);
-		break;
+    case WORD1:
+    case PRONOUN:
+      noun();
+      match(token_type::DESTINATION);
+      verb();
+      tense();
+      match(token_type::PERIOD);
+      break;
 
-	default:
-		syntaxerror2(saved_lexeme, "s3");
-	}
+    default:
+      syntaxerror2(saved_lexeme, "s3");
+    }
 }
 
 // 6 <noun> :: = WORD1 | PRONOUN
 // Done by: Brad
 void noun()
 {
-	cout << "Processing <noun>" << endl;
-	switch (next_token())
-	{
-	case WORD1:
-		match(token_type::WORD1);
-		break;
+  cout << "Processing <noun>" << endl;
+  switch (next_token())
+    {
+    case WORD1:
+      match(token_type::WORD1);
+      break;
 
-	case PRONOUN:
-		match(token_type::PRONOUN);
-		break;
+    case PRONOUN:
+      match(token_type::PRONOUN);
+      break;
 
-	default:
-		syntaxerror2(saved_lexeme, "noun");
-	}
+    default:
+      syntaxerror2(saved_lexeme, "noun");
+    }
 }
 
 // 7 <verb> ::= WORD2
 // Done by: Tony
 void verb()
 {
-	cout << "Processing <verb>" << endl;
-	match(token_type::WORD2);
+  cout << "Processing <verb>" << endl;
+  match(token_type::WORD2);
 }
 
 // 8 <be> :: = IS | WAS
 // Done by: Tony
 void be()
 {
-	cout << "Processing <be>" << endl;
-	switch (next_token())
-	{
-	case IS:
-		match(token_type::IS);
-		break;
+  cout << "Processing <be>" << endl;
+  switch (next_token())
+    {
+    case IS:
+      match(token_type::IS);
+      break;
 
-	case WAS:
-		match(token_type::WAS);
-		break;
+    case WAS:
+      match(token_type::WAS);
+      break;
 
-	default:
-		syntaxerror2(saved_lexeme, "be");
-	}
+    default:
+      syntaxerror2(saved_lexeme, "be");
+    }
 }
 
 // 9 <tense> : = VERBPAST | VERBPASTNEG | VERB | VERBNEG
 // Done by: Tony
 void tense()
 {
-	cout << "Processing <tense>" << endl;
-	switch (next_token())
-	{
-	case VERBPAST:
-		match(token_type::VERBPAST);
-		break;
+  cout << "Processing <tense>" << endl;
+  switch (next_token())
+    {
+    case VERBPAST:
+      match(token_type::VERBPAST);
+      break;
 
-	case VERBPASTNEG:
-		match(token_type::VERBPASTNEG);
-		break;
+    case VERBPASTNEG:
+      match(token_type::VERBPASTNEG);
+      break;
 
-	case VERB:
-		match(token_type::VERB);
-		break;
+    case VERB:
+      match(token_type::VERB);
+      break;
 
-	case VERBNEG:
-		match(token_type::VERBNEG);
-		break;
+    case VERBNEG:
+      match(token_type::VERBNEG);
+      break;
 
-	default:
-		syntaxerror2(saved_lexeme, "tense");
-	}
+    default:
+      syntaxerror2(saved_lexeme, "tense");
+    }
 }
 
 // The test driver to start the parser
@@ -291,23 +290,23 @@ void tense()
 int main()
 {
 
-	//- opens the input file
-	//- calls the <story> to start parsing
-	//- closes the input file 
+  //- opens the input file
+  //- calls the <story> to start parsing
+  //- closes the input file 
 
-	token_type thetype;
-	string theword;
-	
+  token_type thetype;
+  string theword;
+  
 
 
-	string fileName;
-	cout << "please enter a file name" << endl;
-	cin >> fileName;//enter transaction.txt for commands                                                                                                                  
-	fin.open(fileName.data());
+  string fileName;
+  cout << "please enter a file name" << endl;
+  cin >> fileName;//enter transaction.txt for commands                                                                                                                  
+  fin.open(fileName.data());
 
-	story();
-	
-	fin.close();
+  story();
+  
+  fin.close();
 
 }// end
  //** should require no other input files!
--- scanner.cpp
+++ scanner.cpp
@@ -6,145 +6,144 @@ using namespace std;
 
 //=====================================================
 // File scanner.cpp written by: Group Number: 10
-//=====================================================
+//====================================================
 
-enum token_type {ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR};
 
 // ** Done by: TONY
 bool vowel(char letter)
 {
-    const char vow[] = {'a','e','i','o','u','I','E'};
-    for(int i = 0; i < 7; i++)
+  const char vow[] = {'a','e','i','o','u','I','E'};
+  for(int i = 0; i < 7; i++)
     {
-        if(letter == vow[i]) //if the char is in the array
-            return true;
+      if(letter == vow[i]) //if the char is in the array
+	return true;
     }
-    return false;
+  return false;
 }
 // ** Done by: TONY
 bool cons1(char letter)
 {
-    const char cons[] = {'b','g','h','k','m','n','p','r'};
-    for(int i = 0; i < 8; i++)
+  const char cons[] = {'b','g','h','k','m','n','p','r'};
+  for(int i = 0; i < 8; i++)
     {
-        if(letter == cons[i]) //if the char is in the array
-            return true;
+      if(letter == cons[i]) //if the char is in the array
+	return true;
     }
-    return false;
+  return false;
 }
 // ** Done by: TONY 
 bool cons2(char letter)
 {
-    const char cons[] = {'d','j','w','y','z'};
-    for(int i = 0; i < 5; i++)
+  const char cons[] = {'d','j','w','y','z'};
+  for(int i = 0; i < 5; i++)
     {
-        if(letter == cons[i]) //if the char is in the array
-            return true;
+      if(letter == cons[i]) //if the char is in the array
+	return true;
     }
-    return false;
+  return false;
 }
 // ** MYTOKEN DFA to be replaced by the WORD DFA
 // ** Done by: TONY
 // ** RE:
 bool word_dfa(string s)
 {
-    string state = "q0"; //starting state
-    int charpos = 0; //char index
+  string state = "q0"; //starting state
+  int charpos = 0; //char index
     
-    cout << "Trying the word dfa machine " << endl;
+  cout << "Trying the word dfa machine " << endl;
     
-    while (s[charpos] != '\0') //loops through each char in string
+  while (s[charpos] != '\0') //loops through each char in string
     {
-        //the following statements set the states
+      //the following statements set the states
         
-        if (state == "q0" || state == "q0q1")
+      if (state == "q0" || state == "q0q1")
         {
-            if (vowel(s[charpos]))
-                state = "q0q1";
-            else if (state == "q0q1" && s[charpos] == 'n')
-                state = "q0";
-            else if (cons1(s[charpos]))
-                state = "q2q5";
-            else if (cons2(s[charpos]))
-                state = "q5";
-            else if (s[charpos] == 't')
-                state = "q3q5";
-            else if (s[charpos] == 's')
-                state = "q4q5";
-            else if (s[charpos] == 'c')
-                state = "q4";
-            else
-                return(false);
+	  if (vowel(s[charpos]))
+	    state = "q0q1";
+	  else if (state == "q0q1" && s[charpos] == 'n')
+	    state = "q0";
+	  else if (cons1(s[charpos]))
+	    state = "q2q5";
+	  else if (cons2(s[charpos]))
+	    state = "q5";
+	  else if (s[charpos] == 't')
+	    state = "q3q5";
+	  else if (s[charpos] == 's')
+	    state = "q4q5";
+	  else if (s[charpos] == 'c')
+	    state = "q4";
+	  else
+	    return(false);
         }
         
-        else if (state == "q2q5")
+      else if (state == "q2q5")
         {
-            if (vowel(s[charpos]))
-                state = "q0q1";
-            else if (s[charpos] == 'y')
-                state = "q5";
+	  if (vowel(s[charpos]))
+	    state = "q0q1";
+	  else if (s[charpos] == 'y')
+	    state = "q5";
         }
         
-        else if (state == "q3q5")
+      else if (state == "q3q5")
         {
-            if (vowel(s[charpos]))
-                state = "q0q1";
-            else if (s[charpos] == 's')
-                state = "q5";
+	  if (vowel(s[charpos]))
+	    state = "q0q1";
+	  else if (s[charpos] == 's')
+	    state = "q5";
         }
         
-        else if (state == "q4q5")
+      else if (state == "q4q5")
         {
-            if (vowel(s[charpos]))
-                state = "q0q1";
-            else if (s[charpos] == 'h')
-                state = "q5";
+	  if (vowel(s[charpos]))
+	    state = "q0q1";
+	  else if (s[charpos] == 'h')
+	    state = "q5";
         }
         
-        else if (state == "q5")
+      else if (state == "q5")
         {
-            if (vowel(s[charpos]))
-                state = "q0q1";
+	  if (vowel(s[charpos]))
+	    state = "q0q1";
         }
         
-        else if (state == "q4")
+      else if (state == "q4")
         {
-            if (s[charpos] == 'h')
-                state = "q5";
+	  if (s[charpos] == 'h')
+	    state = "q5";
         }
         
-        else
+      else
         {
-            return(false);
+	  return(false);
         }
-        charpos++;
+      charpos++;
     }//end of while
     
-    if (state == "q0" || state == "q0q1")
-        return(true);  // end in a final state
-    else
-        return(false);
+  if (state == "q0" || state == "q0q1")
+    return(true);  // end in a final state
+  else
+    return(false);
 }
 
 // ** Done by: Cam Tran
 
 bool period(string s) {
-    int state = 0;
-    int charpos = 0;
+  int state = 0;
+  int charpos = 0;
     
-    while (s[charpos] != '\0') {
-        if (state == 0 && s[charpos] == '.')
-            state = 1;
-        else
-            return (false);
-        charpos++;
-    } //end of while
+  while (s[charpos] != '\0') {
+    if (state == 0 && s[charpos] == '.')
+      state = 1;
+    else
+      return (false);
+    charpos++;
+  } //end of while
     
     // where did I end up????
-    if (state == 1)
-        return(true);  // end in a final state
-    else
-        return(false);
+  if (state == 1)
+    return(true);  // end in a final state
+  else
+    return(false);
 }
 
 
@@ -152,51 +151,48 @@ bool period(string s) {
 // ** Done by: Cam Tran/Brad
 int scan(token_type& a, string& w, ifstream& fin)
 {
-    fin >> w;
+  fin >> w;
     
-    if (word_dfa(w)) {
-        char lastChar = w[w.length() - 1]; //sets a variable for the last letter to check if WORD1 or WORD2
+  if (word_dfa(w)) {
+    char lastChar = w[w.length() - 1]; //sets a variable for the last letter to check if WORD1 or WORD2
         
-		//checks reserved words
-        if (w == "masu")
-            a = token_type::VERB;
-        else if (w == "masen")
-            a = token_type::VERBNEG;
-        else if (w == "mashita")
-            a = token_type::VERBPAST;
-        else if (w == "masendeshita")
-            a = token_type::VERBPASTNEG;
-        else if (w == "desu")
-            a = token_type::IS;
-        else if (w == "deshita")
-            a = token_type::WAS;
-        else if (w == "o")
-            a = token_type::OBJECT;
-        else if (w == "wa")
-            a = token_type::SUBJECT;
-        else if (w == "ni")
-            a = token_type::DESTINATION;
-        else if (w == "watashi" || w == "anata" || w == "kare" || w == "sore")
-            a = token_type::PRONOUN;
-        else if (w == "mata" || w == "soshite" || w == "shikashi" || w == "dakara")
-            a = token_type::CONNECTOR;
-        else if ((lastChar == 'I') || (lastChar == 'E')) //checks if it ends in capital I or E for WORD2
-            a = token_type::WORD2;
-        else
-            a = token_type::WORD1;
-        return 1;
-    }
-	//runs the period dfa
-    else if (period(w)) {
-        a = token_type::PERIOD;
-        return 1;
-    }
-    else {
-        a = token_type::ERROR;
-        return 0;
-    }
+    //checks reserved words
+    if (w == "masu")
+      a = VERB;
+    else if (w == "masen")
+      a = VERBNEG;
+    else if (w == "mashita")
+      a = VERBPAST;
+    else if (w == "masendeshita")
+      a = VERBPASTNEG;
+    else if (w == "desu")
+      a = IS;
+    else if (w == "deshita")
+      a = WAS;
+    else if (w == "o")
+      a = OBJECT;
+    else if (w == "wa")
+      a = SUBJECT;
+    else if (w == "ni")
+      a = DESTINATION;
+    else if (w == "watashi" || w == "anata" || w == "kare" || w == "sore")
+      a = PRONOUN;
+    else if (w == "mata" || w == "soshite" || w == "shikashi" || w == "dakara")
+      a = CONNECTOR;
+    else if ((lastChar == 'I') || (lastChar == 'E')) //checks if it ends in capital I or E for WORD2
+      a = WORD2;
+    else
+      a = WORD1;
+    return 1;
+  }
+  //runs the period dfa
+  else if (period(w)) {
+    a = PERIOD;
+    return 1;
+  }
+  else {
+    a = ERROR;
+    return 0;
+  }
     
 }//the end
-
-
-
