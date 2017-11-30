c798da08be073b6e3666a180d90b2462363638be
cvtran <cvtran31@gmail.com>
2017-11-30 13:33:29 -0800

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -76,8 +76,6 @@ tokentype next_token()
 	scan(saved_token, lexeme, fin);
 	if (saved_token == ERROR) {
 	  cout << "Lexical Error found in " << lexeme << endl;
-     	token_available = true;
-        // mark that fact that you have saved it
 		saved_lexeme == "eofm";
 	}
      	token_available = true;                  // mark that fact that you have saved it
