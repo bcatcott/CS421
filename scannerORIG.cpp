a5a1892fecef3dbd50be06ce3add01a9abe34778
cvtran <cvtran31@gmail.com>
2017-12-04 13:52:27 -0800

    Update scannerORIG.cpp

--- scannerORIG.cpp
+++ scannerORIG.cpp
@@ -9,120 +9,109 @@ using namespace std;
 
 enum tokentype {ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR};
 
-// ** Done by: TONY
-bool vowel(char letter)
-{
-  const char vow[] = {'a','e','i','o','u','I','E'};
-  for(int i = 0; i < 7; i++)
-    {
-      if(letter == vow[i]) //if the char is in the array
-	return true;
-    }
-  return false;
-}
-// ** Done by: TONY
-bool cons1(char letter)
-{
-  const char cons[] = {'b','g','h','k','m','n','p','r'};
-  for(int i = 0; i < 8; i++)
-    {
-      if(letter == cons[i]) //if the char is in the array
-	return true;
-    }
-  return false;
-}
-// ** Done by: TONY 
-bool cons2(char letter)
-{
-  const char cons[] = {'d','j','w','y','z'};
-  for(int i = 0; i < 5; i++)
-    {
-      if(letter == cons[i]) //if the char is in the array
-	return true;
-    }
-  return false;
-}
 // ** MYTOKEN DFA to be replaced by the WORD DFA
 // ** Done by: TONY
 // ** RE:
 bool word_dfa(string s)
 {
-  string state = "q0"; //starting state
-  int charpos = 0; //char index
-    
-  cout << "Trying the word dfa machine " << endl;
-    
-  while (s[charpos] != '\0') //loops through each char in string
-    {
-      //the following statements set the states
-        
-      if (state == "q0" || state == "q0q1")
-        {
-	  if (vowel(s[charpos]))
-	    state = "q0q1";
-	  else if (state == "q0q1" && s[charpos] == 'n')
-	    state = "q0";
-	  else if (cons1(s[charpos]))
-	    state = "q2q5";
-	  else if (cons2(s[charpos]))
-	    state = "q5";
-	  else if (s[charpos] == 't')
-	    state = "q3q5";
-	  else if (s[charpos] == 's')
-	    state = "q4q5";
-	  else if (s[charpos] == 'c')
-	    state = "q4";
-	  else
-	    return(false);
-        }
-        
-      else if (state == "q2q5")
-        {
-	  if (vowel(s[charpos]))
-	    state = "q0q1";
-	  else if (s[charpos] == 'y')
-	    state = "q5";
-        }
-        
-      else if (state == "q3q5")
-        {
-	  if (vowel(s[charpos]))
-	    state = "q0q1";
-	  else if (s[charpos] == 's')
-	    state = "q5";
-        }
-        
-      else if (state == "q4q5")
-        {
-	  if (vowel(s[charpos]))
-	    state = "q0q1";
-	  else if (s[charpos] == 'h')
-	    state = "q5";
-        }
-        
-      else if (state == "q5")
-        {
-	  if (vowel(s[charpos]))
-	    state = "q0q1";
-        }
-        
-      else if (state == "q4")
-        {
-	  if (s[charpos] == 'h')
-	    state = "q5";
-        }
-        
-      else
-        {
-	  return(false);
-        }
-      charpos++;
-    }//end of while
-    
-  if (state == "q0" || state == "q0q1")
-    return(true);  // end in a final state
-  else
-    return(false);
+	int state = 0;
+	int charpos = 0;
+	//cout << "Inside of the myword function" << endl;
+	while (s[charpos] != '\0')
+	{
+		if (state == 0 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		if (state == 0 && (s[charpos] == 'b' || s[charpos] == 'g' || s[charpos] == 'h' || s[charpos] == 'k' || s[charpos] == 'm' || s[charpos] == 'n' || s[charpos] == 'p' || s[charpos] == 'r'))
+			state = 5;
+		else
+		if (state == 0 && (s[charpos] == 'c'))
+			state = 4;
+		else
+		if (state == 0 && (s[charpos] == 'd' || s[charpos] == 'j' || s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
+			state = 3;
+		else	
+		if (state == 0 && (s[charpos] == 's'))
+			state = 2;
+		else
+		if (state == 0 && (s[charpos] == 't'))
+			state = 1;
+		else
+		if (state == 6 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		if (state == 6 && (s[charpos] == 'n'))
+			state = 7;
+		else
+		if (state == 6 && (s[charpos] == 't'))
+			state = 1;
+		else
+		if (state == 6 && (s[charpos] == 's'))
+			state = 2;
+		else
+		if (state == 6 && (s[charpos] == 'b' || s[charpos] == 'g' || s[charpos] == 'h' || s[charpos] == 'k' || s[charpos] == 'm' || s[charpos] == 'p' || s[charpos] == 'r'))
+			state = 5;
+		else
+		if (state == 6 && (s[charpos] == 'd' || s[charpos] == 'j' || s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
+			state = 3;
+		else
+		if (state == 6 && (s[charpos] == 'c'))
+			state = 4;
+		else
+		if (state == 7 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		if (state == 7 && (s[charpos] == 't'))
+			state = 1;
+		else
+		if (state == 7 && (s[charpos] == 's'))
+			state = 2;
+		else
+		if (state == 7 && (s[charpos] == 'd' || s[charpos] == 'j' || s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
+			state = 3;
+		else
+		if (state == 7 && (s[charpos] == 'b' || s[charpos] == 'g' || s[charpos] == 'h' || s[charpos] == 'k' || s[charpos] == 'm' || s[charpos] == 'n' || s[charpos] == 'p' || s[charpos] == 'r'))
+			state = 5;
+		else
+		if (state == 7 && (s[charpos] == 'c'))
+			state = 4;
+		else
+		if (state == 1 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		if (state == 2 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		if (state == 3 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		if (state == 1 && (s[charpos] == 's'))
+			state = 3;
+		else
+		if (state == 2 && (s[charpos] == 'h'))
+			state = 3;
+		else
+		if (state == 4 && (s[charpos] == 'h'))
+			state = 3;
+		else
+		if (state == 5 && (s[charpos] == 'y'))
+			state = 3;
+		else
+		if (state == 5 && (s[charpos] == 'a' || s[charpos] == 'e' || s[charpos] == 'E' || s[charpos] == 'i' || s[charpos] == 'I' || s[charpos] == 'o' || s[charpos] == 'u'))
+			state = 6;
+		else
+		{
+			return false;
+		}
+
+		charpos++;
+	}//end of while
+	 // where did I end up????
+
+	if (state == 6 || state == 7)
+		return true;  // end in a final state
+	else
+		return false;
 }
 
 // ** Done by: Cam Tran
